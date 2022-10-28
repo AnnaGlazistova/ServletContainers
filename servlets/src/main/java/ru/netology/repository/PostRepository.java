4167d686216fb622616e8dc0a35065aaf5e21c02
Anna Glazistova <glazistovaao@gmail.com>
2022-10-28 10:11:24 +0800

    Init commit

---
+++ servlets/src/main/java/ru/netology/repository/PostRepository.java
@@ -0,0 +1,48 @@
+package ru.netology.repository;
+
+import ru.netology.exception.NotFoundException;
+import ru.netology.model.Post;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+public class PostRepository {
+
+    private final ConcurrentHashMap<Long, Post> mapOfPosts = new ConcurrentHashMap<>();
+
+    private final AtomicLong counter = new AtomicLong(0);
+
+    public List<Post> all() {
+        return new ArrayList<>(mapOfPosts.values());
+    }
+
+    public Optional<Post> getById(long id) {
+        return Optional.ofNullable(mapOfPosts.get(id));
+    }
+
+    public Post save(Post post) {
+        if (post.getId() == 0) {
+            post.setId(counter.getAndIncrement());
+            mapOfPosts.put(post.getId(), post);
+        } else if (mapOfPosts.containsKey(post.getId())) {
+            mapOfPosts.replace(post.getId(), post);
+        } else {
+            throw new NotFoundException("Post does not exist");
+        }
+        return post;
+    }
+
+    public void removeById(long id) {
+        if (mapOfPosts.containsKey(id)) {
+            mapOfPosts.remove(id);
+        } else {
+            throw new NotFoundException("Post does not exist");
+        }
+    }
+}
